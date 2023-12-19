0cf8358421f6a184358fc2520d3f8876846b5f81
Mac Gotsch <mac@acadiantdigital.com>
2023-12-19 15:02:59 -0500

    Updates to images and metadata

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const { hostname } = require("os");
+
 module.exports = {
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol:'https',
+        hostname:'res.cloudinary.com',
+      },
+    ],
   },
 }
