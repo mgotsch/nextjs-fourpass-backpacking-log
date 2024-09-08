622aa0ca5ba4cefe2e35ab648e9affac5cece5ae
Mac Gotsch <mac@acadiantdigital.com>
2024-09-08 17:52:37 -0600

    Addition of google fonts and apply them to appropriate tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,29 @@ import '../styles/index.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
+import { Oswald, Montserrat, Source_Serif_4 } from 'next/font/google'
+
+const oswald = Oswald({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  variable: '--font-oswald',
+});
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+});
+
+const sourceSerif = Source_Serif_4({
+  subsets: ['latin'],
+  variable: '--font-source-serif',
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${oswald.variable} ${montserrat.variable} ${sourceSerif.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
