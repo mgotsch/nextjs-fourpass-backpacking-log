01ac5d2e71711648394b202d1692f960e1306d32
Mac Gotsch <mac@acadiantdigital.com>
2024-09-15 13:20:38 -0600

    Adding animations to the crew member cards to transition them in when they come into view

--- pages/crew/index.tsx
+++ pages/crew/index.tsx
@@ -2,8 +2,11 @@ import Heading from "../../components/Heading";
 import TitleImage from "../../components/TitleImage";
 import CrewMember from "../../components/CrewMember";
 import crewMembers from "../../data/crewMembers";
+import { motion } from "framer-motion";
 
 const Crew = () => {
+  const staggerDelay = 0.2; // Delay between each crew member's animation
+
   return(
     <>
       <Heading title={"Our Crew - Four Days on the Four Pass Loop"} />
@@ -13,11 +16,26 @@ const Crew = () => {
           title="Our Crew"
           subTitle="An intrepid group of some of my favorite folks: nine confirmed people, one possible cyborg, one dog, and a mystical, mysterious, all-powerful force. Wouldn't have been possible (or half as fun) without them all."
         />
-        <div className="team-container p-8">
-          {crewMembers.map((member) => (
-            <CrewMember crewMember={member}/>
+        <motion.div 
+          className="team-container p-8"
+          initial="hidden"
+          animate="visible"
+          variants={{
+            visible: {
+              transition: {
+                staggerChildren: staggerDelay
+              }
+            }
+          }}
+        >
+          {crewMembers.map((member, index) => (
+            <CrewMember 
+              key={member.name} 
+              crewMember={member} 
+              delay={index * staggerDelay}
+            />
           ))}
-        </div>
+        </motion.div>
       </main>
     </>
   );
