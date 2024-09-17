1ccb230ad5f63bda7b5ee1c06256e8d0c898352e
Mac Gotsch <mac@acadiantdigital.com>
2024-09-17 16:59:00 -0600

    Upadte of main page for mobile responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <Heading title={"Four Days on the Four Pass Loop"}/>
       <div className="bg-home h-screen bg-scroll bg-no-repeat bg-cover bg-center backdrop-blur text-slate-200 flex flex-col justify-center items-center">
-        <div className="text-slate-200 flex flex-col justify-center items-center text-center backdrop-blur-sm p-10 border-8 border-white border-double w-4/5 max-w-[700px]">
+        <div className="text-slate-200 flex flex-col justify-center items-center text-center backdrop-blur-sm sm:py-2 md:py-6 lg:py-10 border-8 border-gray-200 border-double w-4/5 max-w-[700px]">
           <h1 className="my-4 text-6xl font-bold uppercase">
             Four Days <div className="md:hidden"></div><span className="text-4xl md:text-5xl">on the</span><div className="h-2"></div>Four Pass Loop
           </h1>
@@ -24,22 +24,22 @@ export default function Home() {
         <div className="h-10"></div>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-10 my-2 justify-center items-center">
           <Link href="/crew">
-            <button className="title-button w-40 backdrop-blur-sm text-slate-200 font-semibold py-2 px-4 border-2 border-white shadow hover:bg-slate-200 hover:text-gray-600 text-nowrap duration-300">
+            <button className="title-button w-40 backdrop-blur-sm text-slate-200 font-semibold py-2 px-4 border-2 border-gray-200 shadow hover:bg-slate-200 hover:text-gray-600 text-nowrap duration-300">
               Our Crew
             </button>
           </Link>
           <Link href="/loop">
-            <button className="title-button w-40 backdrop-blur-sm text-slate-200 font-semibold py-2 px-4 border-2 border-white shadow hover:bg-slate-200 hover:text-gray-600 duration-300">
+            <button className="title-button w-40 backdrop-blur-sm text-slate-200 font-semibold py-2 px-4 border-2 border-gray-200 shadow hover:bg-slate-200 hover:text-gray-600 duration-300">
               The Loop
             </button>
           </Link>
           <Link href="/traillog">
-            <button className="title-button w-40 backdrop-blur-sm text-slate-200 font-semibold py-2 px-4 border-2 border-white shadow hover:bg-slate-200 hover:text-gray-600 duration-300">
+            <button className="title-button w-40 backdrop-blur-sm text-slate-200 font-semibold py-2 px-4 border-2 border-gray-200 shadow hover:bg-slate-200 hover:text-gray-600 duration-300">
               Trail Log
             </button>
           </Link>
           <Link href="/gallery">
-            <button className="title-button w-40 backdrop-blur-sm text-slate-200 font-semibold py-2 px-4 border-2 border-white shadow hover:bg-slate-200 hover:text-gray-600 duration-300">
+            <button className="title-button w-40 backdrop-blur-sm text-slate-200 font-semibold py-2 px-4 border-2 border-gray-200 shadow hover:bg-slate-200 hover:text-gray-600 duration-300">
               Photo Gallery
             </button>
           </Link>
