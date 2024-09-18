042875728ffcf7c50a773c615eebca5f2d6815c9
Mac Gotsch <mac@acadiantdigital.com>
2024-09-18 15:09:35 -0600

    Moving traillog.txt and its read to public directory for Vercel

--- pages/api/read-file.js
+++ pages/api/read-file.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'data', 'trailLog.txt');
+  const filePath = path.join(process.cwd(), 'public', 'trailLog.txt');
   const content = fs.readFileSync(filePath, 'utf-8');
   const paragraphs = content.split('\r\n\r\n');
   res.status(200).json(paragraphs);
