7000f897666b9a78704ef599565a178a5691fd05
Mac Gotsch <mac@acadiantdigital.com>
2024-09-19 19:13:30 -0600

    Adding reference to og-image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,12 +15,15 @@ class MyDocument extends Document {
             content="A collection of photos and writing from the best four days (and the best 4 passes) of the summer of '23."
           />
           <meta property="og:title" content="Four Days on the Four Pass Loop" />
+          <meta property="og:image" content="/og-image.png" />
+
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Four Days on the Four Pass Loop" />
           <meta
             name="twitter:description"
             content="A collection of photos and writing from the best four days (and the best 4 passes) of the summer of '23."
           />
+          <meta name="twitter:image" content="/og-image.png" />
         </Head>
         <body className="bg-black antialiased">
           <Main />
